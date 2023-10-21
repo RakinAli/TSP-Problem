91b64fd6d7da700064e9d337caed244a8eb0e284
Rakin <54189419+RakinAli@users.noreply.github.com>
2023-10-21 07:25:36 +0200

    Quick push

--- Rakin/main.cpp
+++ Rakin/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <algorithm>
+
 
 // Define a point structure
 struct Point
@@ -26,7 +28,7 @@ std::vector<Point> readInput()
 }
 
 // Calculate the Euclidean distance between two points
-double dist(const Point &p1, const Point &p2)
+double distance(const Point &p1, const Point &p2)
 {
   double dx = p1.x - p2.x;
   double dy = p1.y - p2.y;
@@ -46,7 +48,7 @@ std::vector<int> greedyTour(const std::vector<Point> &nodes, int n)
     int best = -1;
     for (int j = 0; j < n; j++)
     {
-      if (!used[j] && (best == -1 || dist(nodes[tour[i - 1]], nodes[j]) < dist(nodes[tour[i - 1]], nodes[best])))
+      if (!used[j] && (best == -1 || distance(nodes[tour[i - 1]], nodes[j]) < distance(nodes[tour[i - 1]], nodes[best])))
       {
         best = j;
       }
@@ -58,7 +60,6 @@ std::vector<int> greedyTour(const std::vector<Point> &nodes, int n)
   return tour;
 }
 
-
 // Print the tour
 void printTour(const std::vector<int> &tour)
 {
@@ -68,10 +69,38 @@ void printTour(const std::vector<int> &tour)
   }
 }
 
+std::vector<int> twoOpt(const std::vector<Point> &nodes, std::vector<int> tour)
+{
+  // Can you build the 2-opt algorithm here. It should take the tour and return a new tour.
+
+  bool finding_tour = true;
+
+  while (finding_tour)
+  {
+    finding_tour = false;
+    for (int i = 0; i < tour.size() - 1; i++)
+    {
+      for (int j = i + 1; j < tour.size(); j++)
+      {
+        double d1 = distance(nodes[tour[i]], nodes[tour[i + 1]]);
+        double d2 = distance(nodes[tour[j]], nodes[tour[(j + 1) % tour.size()]]);
+        double d3 = distance(nodes[tour[i]], nodes[tour[j]]);
+        double d4 = distance(nodes[tour[i + 1]], nodes[tour[(j + 1) % tour.size()]]);
+
+        if (d1 + d2 > d3 + d4)
+        {
+          std::reverse(tour.begin() + i + 1, tour.begin() + j + 1);
+          finding_tour = true;
+        }
+      }
+    }
+  }
+  return tour;
+}
 
 int main(void)
 {
-  std::cout << "Hello, World!" << std::endl;
+  // std::cout << "Hello, World!" << std::endl;
 
   std::vector<Point> nodes = readInput();
   std::cout << "Total nodes: " << nodes.size() << std::endl;
@@ -79,6 +108,12 @@ int main(void)
   // Do a greedy tour
   std::vector<int> tour = greedyTour(nodes, nodes.size());
 
-  // Print the tour
   printTour(tour);
+
+  // std::cout << "2-opt tour: " << std::endl;
+
+  // Print the tour
+  std::vector<int> tour2 = twoOpt(nodes, tour);
+
+  printTour(tour2);
 }
