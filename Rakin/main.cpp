065935059f0f5e1ed68cd3e10476449e4f15eb34
Rakin <54189419+RakinAli@users.noreply.github.com>
2023-11-05 16:19:56 +0100

    Let's try christopides

--- Rakin/main.cpp
+++ Rakin/main.cpp
@@ -305,6 +305,10 @@ std::vector<int> threeOpt(const std::vector<Point> &nodes, std::vector<int> tour
   return tour;
 }
 
+std::vector<int> christopides(const std::vector<Point> &nodes, std::vector<std::vector<double>> distance_matrix, )
+{
+}
+
 int main(void)
 {
   // Read input from stdin and build a distance matrix
@@ -320,14 +324,5 @@ int main(void)
   std::vector<int> tour3 = RRNN(nodes, distance_matrix, 3, 1.925);
 
   // Output the tour
-  // printTour(tour3);
-
-  std::vector<int> tour4 = {52, 565, 575, 25, 185, 345, 750, 945, 685, 845, 655, 880, 660, 25, 230, 525, 1000, 580, 1175, 650, 1130, 1605, 620, 1220, 580, 1465, 200, 1530, 5, 845, 680, 725, 370, 145, 665, 415, 635, 510, 875, 560, 365, 300, 465, 520, 585, 480, 415, 835, 625, 975, 580, 1215, 245, 1320, 315, 1250, 400, 660, 180, 410, 250, 420, 555, 575, 665, 1150, 1160, 700, 580, 685, 595, 685, 610, 770, 610, 795, 645, 720, 635, 760, 650, 475, 960, 95, 260, 875, 920, 700, 500, 555, 815, 830, 485, 1170, 65, 830, 610, 605, 625, 595, 360, 1340, 725, 1740, 245};
-
-  // output the distance of the tour4
-  std::cout << "OPT:: " << tourDistance(nodes, tour4) << std::endl;
-
-  // Output the distance of the tour2
-  std::cout
-      << "RRNN: " << tourDistance(nodes, tour3) << std::endl;
+  printTour(tour3);
 }
